4dda2aa8e769a0b300c825dd751a3778955f5d6b
Sudip-06 <sudipdey9732304182@gmail.com>
2025-08-16 13:35:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -714,5 +714,4 @@ async def analyze_get_info():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
